567dc9570fb78becd020d6da47fca1e37a63da66
dariocon <dariocondepg@gmail.com>
2025-03-28 10:10:06 +0100

    falta limite descripcion

--- src/components/Pelicula.jsx
+++ src/components/Pelicula.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
 
-const Pelicula = ({ pelicula }) => {
+const Pelicula = ({ pelicula, genero }) => {
+    const generoColores = {
+        Acción: 'red',
+        Drama: 'green',
+        Aventura: 'blue',
+      };
+    const colorGenero= generoColores[genero] || 'black';
+    console.log(pelicula.description.slice(0,20))
+    /*const descripcion= pelicula.description;
+    const descripcionList="";
+    if (descripcion.length>20) {
+        descripcionList = descripcion.slice(0,20);
+    }*/
   return (
     <tr key={pelicula.id}>
-      <td>{pelicula.title}</td>
-      <td>{pelicula.description}</td>
+      <td style={{ color: colorGenero }}>{pelicula.title}</td>
+      <td style={{ color: colorGenero }}>{pelicula.description}</td>
+      <td style={{ color: colorGenero }}>{pelicula.genero || "Genero no especificado"} </td>
+
     </tr>
   );
 }
