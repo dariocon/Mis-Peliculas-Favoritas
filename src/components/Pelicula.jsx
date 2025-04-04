999fac13c0665aeb29e957aa2f49d7d755550fe1
dariocon <dariocondepg@gmail.com>
2025-04-04 08:56:23 +0200

    Fase 5

--- src/components/Pelicula.jsx
+++ src/components/Pelicula.jsx
@@ -2,22 +2,37 @@ import { useState } from 'react';
 
 const Pelicula = ({ pelicula, genero }) => {
     const generoColores = {
-        Acción: 'red',
-        Drama: 'green',
-        Aventura: 'blue',
+        "Ciencia Ficción": 'red',
+        "Acción": 'green',
+        "Drama": 'blue',
+        "Comedia": 'grey'
       };
     const colorGenero= generoColores[genero] || 'black';
-    console.log(pelicula.description.slice(0,20))
 
-      let titulo = pelicula.title;
+      let titulo = pelicula.titulo;
       if (titulo.length > 20) {
         titulo = titulo.slice(0, 20) + '...';
       }
   return (
     <tr key={pelicula.id}>
       <td style={{ color: colorGenero }}>{titulo}</td>
-      <td style={{ color: colorGenero }}>{pelicula.description}</td>
+      <td style={{ color: colorGenero }}>{pelicula.descripcion}</td>
       <td style={{ color: colorGenero }}>{pelicula.genero || "Genero no especificado"} </td>
+      <td style={{ color: colorGenero }}> 
+        <ul> 
+        
+        {pelicula.actores.length>0? (
+            pelicula.actores.map((actor) => ( 
+                    
+              <li key={actor}>
+                  {actor}
+              </li>
+
+            ))
+        ):
+          <p>Reparto no disponible </p>
+        } </ul>  </td>
+
 
     </tr>
   );
