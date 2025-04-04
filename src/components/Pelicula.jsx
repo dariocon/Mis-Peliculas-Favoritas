4888618f11ce3dc5a7f8403be038c8a79e75f07a
dariocon <dariocondepg@gmail.com>
2025-04-04 08:11:55 +0200

    fase 4

--- src/components/Pelicula.jsx
+++ src/components/Pelicula.jsx
@@ -8,14 +8,14 @@ const Pelicula = ({ pelicula, genero }) => {
       };
     const colorGenero= generoColores[genero] || 'black';
     console.log(pelicula.description.slice(0,20))
-    /*const descripcion= pelicula.description;
-    const descripcionList="";
-    if (descripcion.length>20) {
-        descripcionList = descripcion.slice(0,20);
-    }*/
+
+      let titulo = pelicula.title;
+      if (titulo.length > 20) {
+        titulo = titulo.slice(0, 20) + '...';
+      }
   return (
     <tr key={pelicula.id}>
-      <td style={{ color: colorGenero }}>{pelicula.title}</td>
+      <td style={{ color: colorGenero }}>{titulo}</td>
       <td style={{ color: colorGenero }}>{pelicula.description}</td>
       <td style={{ color: colorGenero }}>{pelicula.genero || "Genero no especificado"} </td>
 
